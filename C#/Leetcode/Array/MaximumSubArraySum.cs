a8738a85e6a7d342602bf326f1556e0abaea528b
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-06 19:03:58 -0800

    Added solutions for maximums subarray sum and longest substring without repeating characters problems.

---
+++ C#/Leetcode/Array/MaximumSubArraySum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.Array
+{
+    // Leetcode 53
+    class MaximumSubArraySum
+    {
+        // Runtime = 149ms
+        // Tx = O(n)
+        // Sx = O(1)
+        // Greedy Algorithm
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSum = nums[0];
+            int maxSoFar = nums[0];
+
+            for (int index = 1; index < nums.Length; index++)
+            {
+                maxSoFar = Math.Max(maxSoFar + nums[index], nums[index]);
+                maxSum = Math.Max(maxSum, maxSoFar);
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ C#/Leetcode/String/LongestSubStringWithoutRepeatingChars.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.String
+{
+    // Leetcode 3
+    class LongestSubStringWithoutRepeatingChars
+    {
+        // Sliding window technique
+        // Runtime: 336ms (This is changing on resubmission of the same code)
+        // Tx = O(n)
+        // Sx = O(n)
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0 || s.Length == 1)
+                return s.Length;
+
+            int startIndex = 0;
+            int maxLength = 1, currentStrLength = 1;
+            HashSet<char> charsSeenBefore = new HashSet<char>{
+                s[startIndex]
+            };
+
+            for (int endIndex = 1; endIndex < s.Length; endIndex++)
+            {
+                if (!charsSeenBefore.Contains(s[endIndex]))
+                {
+                    currentStrLength++;
+                    charsSeenBefore.Add(s[endIndex]);
+                    maxLength = Math.Max(maxLength, currentStrLength);
+                }
+                else
+                {
+                    startIndex = startIndex + 1;
+                    endIndex = startIndex;
+                    currentStrLength = 1;
+                    charsSeenBefore = new HashSet<char>{
+                        s[startIndex]
+                    };
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
