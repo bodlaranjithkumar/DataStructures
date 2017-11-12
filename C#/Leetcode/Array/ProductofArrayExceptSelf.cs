a8612e0367c9203e44c81687250ddaf1d216227a
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-12 11:14:47 -0800

    LCD-238 : Added solution to product of array except itself.

---
+++ C#/Leetcode/Array/ProductofArrayExceptSelf.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetcodeSolutions.Array
+{
+    // Leetcode 238
+    class ProductofArrayExceptSelf
+    {
+        // Runtime : 472 ms
+        // Tx = O(n) {O(2n), n: length of nums} 
+        // Sx = O(1)
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] output = new int[nums.Length];
+
+            int productSoFar = nums[0];
+            output[0] = 1;
+            int length = nums.Length;
+
+            for (int index = 1; index < length; index++)
+            {
+                output[index] = productSoFar;
+                productSoFar *= nums[index];
+            }
+
+            productSoFar = nums[length - 1];
+            for (int index = length - 2; index >= 0; index--)
+            {
+                output[index] *= productSoFar;
+                productSoFar *= nums[index];
+            }
+
+            return output;
+        }
+    }
+}
