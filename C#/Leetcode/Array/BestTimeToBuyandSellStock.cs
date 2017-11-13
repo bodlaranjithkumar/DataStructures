485f0c56499d20ee79dde194f628d165b7c07c77
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-13 14:08:01 -0800

    LCD-121 : Added solution for best time to buy and sell stocks problem.

---
+++ C#/Leetcode/Array/BestTimeToBuyandSellStock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetcodeSolutions.Array
+{
+    // Leetcode 121
+    class BestTimeToBuyandSellStock
+    {
+        // Runtime = 172ms
+        // Tx = O(n) {n:length of the array}
+        // Sx = O(1) 
+        // Greedy Algorithm
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length < 2)
+            {
+                return 0;
+            }
+
+            int min = prices[0];
+            int maxProfit = prices[1] - prices[0];
+
+            for (int index = 1; index < prices.Length; index++)
+            {
+                maxProfit = Math.Max(maxProfit, prices[index] - min);
+
+                min = Math.Min(min, prices[index]);
+            }
+
+            // Given : Return 0 when the stock price is decreasing constantly.
+            return maxProfit < 0 ? 0 : maxProfit;
+        }
+    }
+}
