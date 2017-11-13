c16df0882b57347935df260697d8bef03eaef2e5
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-13 15:36:24 -0800

    LCD-152 : Added solution for maximum subarray product problem.

---
+++ C#/Leetcode/Array/MaximumSubArrayProduct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetcodeSolutions.Array
+{
+    // Leetcode 152
+    class MaximumSubArrayProduct
+    {
+        // Runtime = 155ms
+        // Tx = O(n)
+        // Sx = O(1)
+        // Greedy Algorithm. Key is to swap the computed values when the current value is -ve 
+        // This allows us to get max value when even -ve values are present.
+        public int MaxProduct(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int maxProduct = nums[0];
+            int maxSoFar = maxProduct, minSoFar = maxProduct;
+
+            for (int index = 1; index < nums.Length; index++)
+            {
+                // swap the values so that when we find even number of -ve value, we get the max product.
+                if (nums[index] < 0)
+                {
+                    int temp = maxSoFar;
+                    maxSoFar = minSoFar;
+                    minSoFar = temp;
+                }
+
+                maxSoFar = Math.Max(nums[index], maxSoFar * nums[index]);
+                minSoFar = Math.Min(nums[index], minSoFar * nums[index]);
+
+                maxProduct = Math.Max(maxProduct, maxSoFar);
+            }
+
+            return maxProduct;
+        }
+    }
+}
