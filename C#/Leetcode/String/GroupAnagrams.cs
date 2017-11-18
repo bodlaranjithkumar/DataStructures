7a5eae91eaafe5996edc67685cbab23ccad1dfd6
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-18 12:18:35 -0800

    LCD-49: Added solution to group anagrams problem.

---
+++ C#/Leetcode/String/GroupAnagrams.cs
@@ -0,0 +1,38 @@
+﻿using sys = System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetcodeSolutions.String
+{
+    // Leetcode 49
+    class GroupAnagrams
+    {
+        // Input = ["eat", "tea", "tan", "ate", "nat", "bat"]
+        // Output = [ ["ate", "eat","tea"], ["nat","tan"], ["bat"]]
+        // Runtime = 545 ms
+        // Tx = O(m * nlogn) {m: length of strs, n: length of a string in the collection}
+        // Sx = O(m)
+        public IList<IList<string>> GroupAnagramss(string[] strs)
+        {
+            Dictionary<string, IList<string>> result = new Dictionary<string, IList<string>>();
+
+            foreach (string str in strs)
+            {
+                char[] chars = str.ToCharArray();
+                sys.Array.Sort(chars);
+                string sortedStr = new sys.String(chars);
+
+                if (result.Keys.Contains(sortedStr))
+                {
+                    result[sortedStr].Add(str);
+                }
+                else
+                {
+                    result[sortedStr] = new List<string> { str };
+                }
+            }
+
+            return result.Values.ToList();
+        }
+    }
+}
