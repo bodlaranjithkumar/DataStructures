129ea43e6a9bdf0e9c3fd2360c6df432d00f3cd4
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-18 11:08:10 -0800

    LCD-5 : Added solution to the longest palindromic substring problem.

---
+++ C#/Leetcode/String/LongestPalindromicSubstring.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LeetcodeSolutions.String
+{
+    // Leetcode 5
+    class LongestPalindromicSubstring
+    {
+        // Runtime = 159ms
+        // Tx = O(n^2)
+        // Sx = O(1)
+        // Reference : https://discuss.leetcode.com/topic/23498/very-simple-clean-java-solution
+        public string LongestPalindrome(string s)
+        {
+            int length = s.Length;
+            if (length < 2)
+                return s;
+
+            int start = 0, maxLen = 0;
+
+            for (int index = 0; index < length - 1; index++)
+            {
+                ValidatePalindrome(s, index, index, ref start, ref maxLen); // assume length of palindrome is odd
+                ValidatePalindrome(s, index, index + 1, ref start, ref maxLen); // assume lenth of palindromei cs even
+            }
+
+            return s.Substring(start, maxLen);
+        }
+
+        private void ValidatePalindrome(string s, int leftIndex, int rightIndex, ref int start, ref int maxLen)
+        {
+            while (leftIndex >= 0 && rightIndex < s.Length && s[leftIndex] == s[rightIndex])
+            {
+                leftIndex--;
+                rightIndex++;
+            }
+
+            if (maxLen < rightIndex - leftIndex - 1)
+            {
+                start = leftIndex + 1;
+                maxLen = rightIndex - leftIndex - 1;
+            }
+        }
+    }
+}
