50459da69901d1667cf1f71225494e021a0655a6
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-18 14:48:36 -0800

    LCD-70: Added solution to climbing stairs problem.

---
+++ C#/Leetcode/Math/ClimbingStairs.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetcodeSolutions.Math
+{
+    // Leetcode 70
+    class ClimbingStairs
+    {
+        // 1 - 1
+        // 2 - 2
+        // 3 - 3 (1 + 2)
+        // 4 - 5 (2 + 3)
+        // 5 - 8 (3 + 5) .......
+        // Runtime = 150ms
+        // Tx = O(n) {n = input}
+        // Sx = O(1)
+        public int ClimbStairs(int n)
+        {
+            if (n == 0 || n == 1 || n == 2)
+            {
+                return n;
+            }
+
+            int prev = 2;
+            int prevPrev = 1;
+            int numberOfWays = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                numberOfWays = prev + prevPrev;
+                prevPrev = prev;
+                prev = numberOfWays;
+            }
+
+            return numberOfWays;
+        }
+    }
+}
