df00ec8020256c3cc43269c998d272af4116adfd
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-18 12:41:13 -0800

    LCD-141: Added solution to linked list cycle problem.

---
+++ C#/Leetcode/LinkedList/LinkedListCycle.cs
@@ -0,0 +1,29 @@
+﻿using LeetcodeSolutions.DataStructures;
+
+namespace LeetcodeSolutions.LinkedList
+{
+    // Leetcode 141
+    class LinkedListCycle
+    {
+        // Runtime = 146 ms
+        // Tx = O(n)
+        // Sx = O(1)
+        // Slow runner and fast runner technique.
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (slow != null && fast != null && fast.Next != null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+
+                if (slow == fast)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
