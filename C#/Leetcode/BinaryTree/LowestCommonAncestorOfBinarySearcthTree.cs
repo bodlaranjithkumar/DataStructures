ecb9f2f2cb5563464080add276075e642e8475d6
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-19 16:26:52 -0800

    LCD-235 : Added solution for Lowest Common Ancestor Of a Binary Search Tree problem.

---
+++ C#/Leetcode/BinaryTree/LowestCommonAncestorOfBinarySearcthTree.cs
@@ -0,0 +1,55 @@
+﻿using LeetcodeSolutions.DataStructures;
+
+namespace LeetcodeSolutions.BinaryTree
+{
+    // Leetcode 235
+    class LowestCommonAncestorOfBinarySearcthTree
+    {
+        // Runtime = 209 ms
+        // Tx = O(h) {h: height of the BST}
+        // Sx = O(1)
+        // Assumption : p,q nodes exists in the BST.
+        public BinaryTreeNode LowestCommonAncestor(BinaryTreeNode root, BinaryTreeNode p, BinaryTreeNode q)
+        {
+            if (root == null || p == null || q == null)
+                return null;
+
+            while (true)
+            {
+                if (root.Val < p.Val && root.Val < q.Val)
+                {
+                    root = root.Right;
+                }
+                else if (root.Val > p.Val && root.Val > q.Val)
+                {
+                    root = root.Left;
+                }
+                else
+                {
+                    return root;
+                }
+            }
+        }
+
+        // Tx = O(h)
+        // Sx = O(h) {for call stack}
+        public BinaryTreeNode LowestCommonAncestorRecursive(BinaryTreeNode root, BinaryTreeNode p, BinaryTreeNode q)
+        {
+            if (root == null || p == null || q == null)
+                return null;
+
+            if (root.Val < p.Val && root.Val < q.Val)
+            {
+                return LowestCommonAncestorRecursive(root.Right,p,q);
+            }
+            else if (root.Val > p.Val && root.Val > q.Val)
+            {
+                return LowestCommonAncestorRecursive(root.Left, p, q);
+            }
+            else
+            {
+                return root;
+            }
+        }
+    }
+}
