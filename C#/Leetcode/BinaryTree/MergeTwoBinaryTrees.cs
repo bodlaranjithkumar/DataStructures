f0b425b0b04ecae1d273e6ba1a42577f48d2521f
rbodla <bodlaranjithkumar@hotmail.com>
2017-11-20 23:35:24 -0800

    LCD-617: Added solution to Merge Two Binary Tree(Add) problem.

---
+++ C#/Leetcode/BinaryTree/MergeTwoBinaryTrees.cs
@@ -0,0 +1,27 @@
+﻿using LeetcodeSolutions.DataStructures;
+using System;
+
+namespace LeetcodeSolutions.BinaryTree
+{
+    // Leetcode 617
+    class MergeTwoBinaryTrees
+    {
+        // Runtime = 212 ms
+        // Tx = O(n)
+        // Sx = O(n) for the recursive call stack
+        public BinaryTreeNode MergeTrees(BinaryTreeNode t1, BinaryTreeNode t2)
+        {
+            if (t1 == null && t2 == null)    // leaf node
+                return null;
+            else if (t1 == null)
+                t1 = new BinaryTreeNode(t2.Val);
+            else if (t1 != null && t2 != null)
+                t1.Val += t2.Val;
+
+            t1.Left = MergeTrees(t1.Left, t2?.Left);
+            t1.Right = MergeTrees(t1.Right, t2?.Right);
+
+            return t1;
+        }
+    }
+}
