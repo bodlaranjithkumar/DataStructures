fb7fff39cc60f2ad8c92566c924b73680256b6b9
rbodla <bodlaranjithkumar@hotmail.com>
2017-12-14 19:52:22 -0800

    Added solution to reverse string preserving the position of special characters.

---
+++ C#/Leetcode/Interview/AddSpacesBetweenCharacters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.Interview
+{
+    // Input : " coding  is fu n   "
+    // Output: " c o d i n g  i s f u n   "
+    public class AddSpacesBetweenCharacters
+    {
+        //public static void Main(string[] args)
+        //{
+        //    string input1 = " coding  is fu n   ";
+        //    Console.WriteLine($"input:{input1}.\t expected:{" c o d i n g  i s f u n   "}.\t output:{AddSpaces(input1)}.");
+
+        //    string input2 = "s  ";
+        //    Console.WriteLine($"input:{input2}.\t expected:{input2}.\t output:{AddSpaces(input2)}.");
+
+        //    string input3 = "";
+        //    Console.WriteLine($"input:{input3}.\t expected:{input3}.\t output:{AddSpaces(input3)}.");
+
+        //    Console.ReadKey();
+        //}
+
+        public static string AddSpaces(string input)
+        {
+            if (string.IsNullOrEmpty(input) || input.Length == 1)
+                return input;
+
+            StringBuilder output = new StringBuilder();
+
+            for(int index = 0; index < input.Length; index++)
+            {
+                char charAtIndex = input[index];
+                bool charAtNextIndexIsNotSpace = false;
+                
+                if (charAtIndex != ' ')
+                {
+                    if (index < input.Length - 1 && input[index + 1] != ' ')
+                        charAtNextIndexIsNotSpace = true;
+
+                    output.Append(charAtIndex);
+                }   
+
+                if (charAtNextIndexIsNotSpace || charAtIndex == ' ')
+                    output.Append(' ');
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ C#/Leetcode/String/ReverseStringPreservingSpecialCharacterPosition.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.String
+{
+    public class ReverseStringPreservingSpecialCharacterPosition
+    {
+        public static void Main(string[] args)
+        {
+            string str1 = "b$";
+            Console.WriteLine($"input:{str1}, expected:{str1}, actual:{ReverseString(str1)}");
+
+            string str2 = "ab";
+            Console.WriteLine($"input:{str2}, expected:ba, actual:{ReverseString(str2)}");
+
+            string str3 = "abc$de%f";
+            Console.WriteLine($"input:{str3}, expected:fed$cb%a, actual:{ReverseString(str3)}");
+
+            string str4 = "a(&fdgh*fdf";
+            Console.WriteLine($"input:{str4}, expected:f(&dfhg*dfa, actual:{ReverseString(str4)}");
+
+            Console.ReadKey();
+        }
+
+        //Tx = O(n)
+        //Sx = O(n)
+        public static string ReverseString(string input)
+        {
+            //edge cases
+            if (string.IsNullOrEmpty(input) || input.Length == 1)
+                return input;
+
+            char[] chars = input.ToCharArray();
+            int left = 0;
+            int right = input.Length - 1;
+
+            while (left < right)
+            {
+                while (left < right && !Char.IsLetterOrDigit(chars[left]))
+                {
+                    left++;
+                }
+
+                while (left < right && !Char.IsLetterOrDigit(chars[right]))
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    char temp = chars[left];
+                    chars[left] = chars[right];
+                    chars[right] = temp;
+
+                    left++;
+                    right--;
+                }
+            }
+
+            return new string(chars);
+        }
+    }
+}
