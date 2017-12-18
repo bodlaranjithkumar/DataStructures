eab20576fe2fe4e5bf44dcdb597915b080c1f4b5
rbodla <bodlaranjithkumar@hotmail.com>
2017-12-18 17:30:39 -0500

    LCD-251: Added optimized solution for reverse a string problem.

---
+++ C#/Leetcode/test1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeSolutions
+{
+    public class test1
+    {
+
+        //public static void Main(string[] args)
+        //{
+        //    //Console.WriteLine($"{IsInterestingPoint("222221")}");
+        //    //Console.WriteLine($"{IsInterestingPoint("222123")}");
+        //    string s = "01:21:21";
+        //    string[] stringStart = s.Split(new[] { ':' });
+        //    int[] start = stringStart.Select(int.Parse).ToArray();
+
+        //    string T = "22:21:23";
+        //    string[] stringEnd = T.Split(new[] { ':' });
+        //    int[] end = stringEnd.Select(int.Parse).ToArray();
+
+        //    int TotalInterestingPoints = 0;
+
+        //    for (int h = start[0]; h <= end[0]; h++)
+        //    {
+        //        int m = start[1];
+        //        if (h != start[0])  // start m with 0 if h is start hour
+        //            m = 0;
+
+        //        for (; m <= end[1] && m < 59; m++)
+        //        {
+        //            int ss = start[2];
+        //            if (m != start[1])   // start ss with 0 if m is start minute
+        //                ss = 0;
+
+        //            for (; ss <= end[2] && ss < 59; ss++)
+        //            {
+        //                StringBuilder time = new StringBuilder();
+
+        //                CustomAppend(time, h);
+        //                CustomAppend(time, m);
+        //                CustomAppend(time, ss);
+
+        //                if (IsInterestingPoint(time.ToString()))
+        //                    TotalInterestingPoints++;
+        //            }
+        //        }
+        //    }
+
+        //    Console.WriteLine($"{TotalInterestingPoints}");
+
+        //    Console.ReadKey();
+        //}
+
+        private static void CustomAppend(StringBuilder time, int t)
+        {
+            if (t >= 0 && t < 9)
+                time.Append(0);
+
+            time.Append(t);
+        }
+
+        private static bool IsInterestingPoint(string time)
+        {
+            HashSet<int> visited = new HashSet<int>();
+
+            foreach(char c in time)
+            {
+                int number = c - '0';
+                if (!visited.Contains(number))
+                {
+                    if (visited.Count == 2)
+                        return false;
+                    else
+                        visited.Add(number);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C#/Leetcode/teste2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewCakeSolutions
+{
+    class Solution
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("computedvalue: {0}", ComputeValue("11100"));
+        //    Console.WriteLine("Totalsteps: {0}", CaculateTotalSteps(ComputeValue("11100")));
+
+        //    Console.ReadKey();
+        //}
+
+        //public int solution(string S)
+        //{
+        //    // write your code in C# 6.0 with .NET 4.5 (Mono)
+        //    Console.WriteLine("computedvalue: {0}", ComputeValue("0100"));
+        //    return 0;
+        //}
+
+        private static int CaculateTotalSteps(ulong value)
+        {
+            int totalSteps = 0;
+            while (value > 0)
+            {
+                if (value % 2 == 0)
+                    value /= 2;
+                else
+                    value--;
+
+                totalSteps++;
+            }
+
+            return totalSteps;
+        }
+
+        // 4 = 0100
+        private static ulong ComputeValue(string S)
+        {
+            ulong value = 0;
+            int count = 0;
+
+            int index = S.Length - 1;
+            while (index > 0)
+            {
+                value += S[index] - '0' == 0 ? 0 : (ulong)Math.Pow(2, count);
+                count++;
+                index--;
+            }
+
+            return value;
+        }
+    }
+}
