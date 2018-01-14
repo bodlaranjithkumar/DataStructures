d61c6815bdd6ee1048ef621511868d8d54528bec
rbodla <bodlaranjithkumar@hotmail.com>
2018-01-14 14:02:47 -0800

    LCD-19 : Added solution to remove nth node from end of the list.

---
+++ C#/Leetcode/LinkedList/RemoveNthNodeFromEndofList.cs
@@ -0,0 +1,40 @@
+﻿using LeetcodeSolutions.DataStructures;
+
+namespace LeetcodeSolutions.LinkedList
+{
+    // Leetcode 19
+    public class RemoveNthNodeFromEndofList
+    {
+        // Tx = O(n) {n : number of nodes in the linked list}
+        // Sx = O(1)
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode slower = head;
+            ListNode faster = head;
+
+            ListNode prev = new ListNode(0)
+            {
+                Next = head
+            };
+            ListNode resultNode = prev;
+
+
+            while (n > 1 && faster.Next != null)
+            {
+                faster = faster.Next;
+                n--;
+            }
+
+            while (faster.Next != null)
+            {
+                faster = faster.Next;
+                prev = prev.Next;
+                slower = slower.Next;
+            }
+
+            prev.Next = slower.Next;
+
+            return resultNode.Next;
+        }
+    }
+}
