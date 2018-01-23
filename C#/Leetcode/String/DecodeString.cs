887b4315ba99cbea5e06e3ce1c78b93cf417d81d
rbodla <bodlaranjithkumar@hotmail.com>
2018-01-22 21:44:55 -0800

    LCD-394: Added solution to Decode String prolem.

---
+++ C#/Leetcode/String/DecodeString.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.String
+{
+    // Leetcode 394
+    public class DecodeString
+    {
+        //public static void Main(string[] args)
+        //{
+        //    DecodeString d = new DecodeString();
+        //    var result1 = d.DecodeEncodedString("3[a2[c]]");
+        //    var result2 = d.DecodeEncodedString("2[abc]3[cd]ef");
+        //    var result3 = d.DecodeEncodedString("3[a]2[bc]");
+        //    var result4 = d.DecodeEncodedString("3[a2[c]]2[b]");
+        //    var result5 = d.DecodeEncodedString("100[a]");
+        //}
+
+        //3[a2[c]]
+        //2[abc]3[cd]ef
+        //3[a]2[bc]
+        //3[a2[c]]2[b]
+        //100[a]
+        // Runtime: 158ms
+
+        public string DecodeEncodedString(string s)
+        {
+            Stack<int> frequencies = new Stack<int>();
+            Stack<char> chars = new Stack<char>();
+
+            StringBuilder decodedString = new StringBuilder();
+
+            for (int j = 0; j < s.Length; j++)
+            {
+                char c = s[j];
+
+                if (char.IsDigit(c))
+                {
+                    int count = 0;
+                    while (s[j] >= '0' && s[j] <= '9')
+                    {
+                        count = count * 10 + s[j] - '0';
+                        j++;
+                    }
+
+                    j--;
+                    frequencies.Push(count);
+                }
+                else if (char.IsLetter(c) || c == '[')
+                {
+                    if (frequencies.Count == 0)
+                        decodedString.Append(c);    // This is a letter since the assumption is the string is well formed.
+                    else
+                        chars.Push(c);  // letter or [
+                }
+                else if (c == ']')
+                {
+                    int frequency = frequencies.Pop();
+
+                    StringBuilder str = new StringBuilder();
+                    while (Char.IsLetter(chars.Peek()))
+                    {
+                        char charToAppend = chars.Pop();
+                        str.Insert(0, charToAppend);    // append chars in the beginning.
+                    }
+
+                    chars.Pop();    // Remove [
+
+                    for (int i = 0; i < frequency; i++)
+                    {
+                        foreach (char charToInsert in str.ToString())
+                            chars.Push(charToInsert);
+                    }
+
+                    // Append to the decoded string.
+                    if (frequencies.Count == 0)
+                    {
+                        if (decodedString.Length == 0 && j != s.Length-1)
+                        {
+                            while (chars.Count > 0)
+                                decodedString.Insert(0, chars.Pop());
+                        }
+                        else
+                        {
+                            int length = decodedString.Length;
+                            while (chars.Count > 0)
+                                decodedString.Insert(length, chars.Pop());
+                        }
+                    }
+                } 
+            }
+
+            return decodedString.ToString();
+        }
+    }
+}
