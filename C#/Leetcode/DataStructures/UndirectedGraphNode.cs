3019242e21c017facc91de43acea7c3f845b0ba7
rbodla <bodlaranjithkumar@hotmail.com>
2018-01-29 22:58:38 -0800

    LCD-133: Added Graph Datastructure and solution to Clone Graph problem.

---
+++ C#/Leetcode/DataStructures/UndirectedGraphNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace LeetcodeSolutions.DataStructures
+{
+    public class UndirectedGraphNode
+    {
+        public int label { get; set; }
+        public IList<UndirectedGraphNode> neighbors { get; set; }
+
+        public UndirectedGraphNode(int value)
+        {
+            label = value;
+            neighbors = new List<UndirectedGraphNode>();
+        }
+    }
+}
