3c96220c3f0dcd39fbf673bdb981e9ead095f1c4
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-07 23:43:39 -0800

    LCD-298: Added solution to find Longest Consecutive Sequence in Binary Tree.

---
+++ C#/Leetcode/BinaryTree/LongestConsecutiveSequence.cs
@@ -0,0 +1,70 @@
+﻿using LeetcodeSolutions.DataStructures;
+using System;
+
+namespace LeetcodeSolutions.BinaryTree
+{
+    // Leetcode 298
+    public class LongestConsecutiveSequence
+    {
+        private int max;
+
+        //public static void Main(string[] args)
+        //{
+        //    LongestConsecutiveSequence ls = new LongestConsecutiveSequence();
+
+        //    BinaryTreeNode root = new BinaryTreeNode(6);
+        //    root.Right = new BinaryTreeNode(9)
+        //    {
+        //        Left = new BinaryTreeNode(7),
+        //        Right = new BinaryTreeNode(10)
+        //    };
+        //    root.Right.Right.Right = new BinaryTreeNode(11);
+
+        //    Console.WriteLine(ls.FindLongestConsecutiveSequence(root));
+
+        //    BinaryTreeNode root1 = new BinaryTreeNode(1);
+        //    root1.Left = new BinaryTreeNode(2)
+        //    {
+        //        Left = new BinaryTreeNode(3)
+        //    };
+
+        //    root1.Right = new BinaryTreeNode(4)
+        //    {
+        //        Left = new BinaryTreeNode(5),
+        //        Right = new BinaryTreeNode(6)
+        //    };
+        //    root1.Right.Right.Left = new BinaryTreeNode(7);
+
+        //    Console.WriteLine(ls.FindLongestConsecutiveSequence(root1));
+        //    Console.ReadKey();
+        //}
+
+        // Tx = O(n)
+        // Sx = O(d)    For call stack
+        public int FindLongestConsecutiveSequence(BinaryTreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            FindLongestConsecutiveSequence(root, 0, root.Val);
+
+            return max;
+        }
+
+        public void FindLongestConsecutiveSequence(BinaryTreeNode node, int length, int nodeValue)
+        {
+            if (node == null)
+                return;
+
+            if (node.Val == nodeValue)
+                length++;
+            else
+                length = 1;
+
+            max = System.Math.Max(max, length);
+
+            FindLongestConsecutiveSequence(node.Left, length, node.Val + 1);
+            FindLongestConsecutiveSequence(node.Right, length, node.Val + 1);
+        }
+    }
+}
