041eaa0bf5afebaa2ce2a753f86a04bab33b087b
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-10 23:12:49 -0800

    LCD404 : Added solution to calculate sum of left leaves problem.

---
+++ C#/Leetcode/BinaryTree/SumOfLeftLeaves.cs
@@ -0,0 +1,31 @@
+﻿using LeetcodeSolutions.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.BinaryTree
+{
+    // Leetcode 404
+    public class SumOfLeftLeaves
+    {
+        // Tx = O(n)
+        // Sx = O(d)
+        public int CalculateSumOfLeftLeaves(BinaryTreeNode node)
+        {
+            if (node == null) return 0;
+
+            int sum = 0;
+            if (node.Left != null)
+            {
+                if (node.Left.Left == null && node.Left.Right == null)
+                    sum += node.Left.Val;
+                else
+                    sum += CalculateSumOfLeftLeaves(node.Left);
+            }
+
+            sum += CalculateSumOfLeftLeaves(node.Right);
+
+            return sum;
+        }
+    }
+}
