47ad9650b9dfc1ce95ab235119b46025e2529fcb
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-10 22:20:11 -0800

    LCD-206: Added solution to reverse linked list in place problem.

---
+++ C#/Leetcode/LinkedList/ReverseLinkedList.cs
@@ -0,0 +1,28 @@
+﻿using LeetcodeSolutions.DataStructures;
+
+namespace LeetcodeSolutions.LinkedList
+{
+    // Leetcode 206
+    public class ReverseLinkedList
+    {
+        // Tx = O(n)
+        // Sx = O(1)
+        // Inplace algorithm
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode current = head;
+            ListNode previous = null;
+            ListNode nextNode = null;
+
+            while (current != null)
+            {
+                nextNode = current.Next;
+                current.Next = previous;
+                previous = current;
+                current = nextNode;
+            }
+
+            return previous;
+        }
+    }
+}
