d2f588db9692d5e54d12f678a7b955c8d7763f7a
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-11 11:07:23 -0800

    Added solution for find sum of all binary tree leaves.

---
+++ C#/Leetcode/BinaryTree/SumOfLeaves.cs
@@ -0,0 +1,28 @@
+﻿using LeetcodeSolutions.DataStructures;
+
+namespace LeetcodeSolutions.BinaryTree
+{
+    //https://www.geeksforgeeks.org/sum-leaf-nodes-binary-tree/
+    public class SumOfLeaves
+    {
+        // Tx = O(n)
+        // Sx = O(d)
+        public int LeavesSum(BinaryTreeNode root)
+        {
+            if (root == null) return 0;
+
+            int sum = 0;
+            if (root.Left == null && root.Right == null)
+            {
+                sum += root.Val;
+            }
+            else
+            {
+                sum += LeavesSum(root.Left);
+                sum += LeavesSum(root.Right);
+            }
+
+            return sum;
+        }
+    }
+}
