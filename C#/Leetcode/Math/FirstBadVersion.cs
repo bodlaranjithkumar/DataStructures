f2ffbec4993853bc9e095a1655aebf113f262484
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-10 18:30:05 -0800

    LCD-278: Added solution to First Bad Version problem using Binary Search.

---
+++ C#/Leetcode/Math/FirstBadVersion.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetcodeSolutions.Math
+{
+    // Leetcode 278
+    // Modified binary search
+    public class FirstBadVersion //: VersionControl
+    {
+        //Tx = O(lgn)
+        public int FindFirstBadVersion(int n)
+        {
+            int low = 1, high = n;
+
+            while (low < high)
+            {
+                int mid = low + (high - low) / 2;
+
+                // IsBadVersion below is implemented by OJ.
+                //if (IsBadVersion(mid)) high = mid;
+                //else low = mid + 1;
+            }
+
+            return low;
+        }
+
+        // Tx = O(n)
+        public int FindFirstBadVersionBruteForce(int n)
+        {
+            for (int i = 1; i < n; i++)
+               // if (IsBadVersion(i))
+                    return i;
+
+            return n;
+        }
+    }
+}
