f65c0cae2a871fd0c5494da1f735b5c788c6340d
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-10 22:07:36 -0800

    LCD-680: Added partially working solution for Valid Palindrome II problem.

---
+++ C#/Leetcode/String/ValidPalindromeII.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetcodeSolutions.String
+{
+    // Leetcode 680
+    public class ValidPalindromeII
+    {
+        // Tx = O(n)
+        // Sx = O(1)
+        // Failed for the input "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"
+        // Actual: false, expected: true
+        public bool ValidPalindrome(string s)
+        {
+            int count = 0, low = 0, high = s.Length - 1;
+
+            while (low < high)
+            {
+                if (s[low] != s[high])
+                {
+                    if (count == 1)
+                        return false;
+
+                    count++;
+
+                    if (low + 1 < high && s[low + 1] == s[high])
+                        low++;
+                    else //if(low < high-1 && s[low] == s[high-1])
+                        high--;
+                }
+                else
+                {
+                    low++;
+                    high--;
+                }
+            }
+
+            return true;
+        }
+    }
+}
