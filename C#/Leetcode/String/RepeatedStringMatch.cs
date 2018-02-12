b8689442ce0cbe883d5dd49bcad9eab54414c73e
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-11 16:26:57 -0800

    LCD-686: Added solution to repeated string match problem.

---
+++ C#/Leetcode/String/RepeatedStringMatch.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace LeetcodeSolutions.String
+{
+    // Leetcode 686
+    // Submission : https://leetcode.com/submissions/detail/140491920/
+    public class RepeatedStringMatch
+    {
+        // aa,a
+        // abcabcabcabc,abac
+        // abababaaba, aabaaba
+        // abcd, cdabcdab
+        public int IsRepeatedStringMatch(string A, string B)
+        {
+            int count = 1;
+            StringBuilder sb = new StringBuilder(A);
+            while (sb.Length < B.Length)    //Key: Run until A's length is less that B's length
+            {
+                sb.Append(A);
+                count++;
+            }
+
+            if (sb.ToString().Contains(B)) return count;
+            else if (sb.Append(A).ToString().Contains(B)) return count + 1;
+            else return -1;
+        }
+    }
+}
