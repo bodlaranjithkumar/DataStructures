ab7f41d66f33a73a0bb4bf26522e6930275ab95a
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-15 23:30:42 -0800

    LCD-389: Added solution to Find the only one different character in two strings.

---
+++ C#/Leetcode/String/FindTheDifference.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetcodeSolutions.String
+{
+    // Leetcode 389
+    // Submission: https://leetcode.com/submissions/detail/141070720/
+    public class FindTheDifference
+    {
+        // Calculate the ASCII value difference.
+        // Tx = O(n)
+        // Sx = O(1)
+        public char FindTheAdditionalCharacter(string s, string t)
+        {
+            // Initialize with t's last character ASCII value because 
+            // the for loop runs for length of s.
+            int difference = t[t.Length-1];
+
+            for(int i = 0; i < s.Length; i++)
+                difference = difference - s[i] + t[i];
+
+            return (char)difference;
+        }
+    }
+}
