54d9c0aa83fa8b8ce079927de2caf7051057a59f
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-15 22:47:33 -0800

    LCD-155: Added optimized solution for Min Stack problem.

--- C#/Leetcode/Stack/MinStack.cs
+++ C#/Leetcode/Stack/MinStack.cs
@@ -12,50 +12,99 @@ namespace LeetcodeSolutions.Stack
     //      minStack.pop();
     //      minStack.top();      --> Returns 0.
     //      minStack.getMin();   --> Returns -2.
+
+    // Submission: https://leetcode.com/submissions/detail/141066933/
+    // Use custom MinStackElement class with the number, min so far properties.
+    // Sx = O(2n)
     public class MinStack
     {
-        private Stack<int> minimumStack;
-        private Stack<int> stack;
-        private Stack<int> minimumStackIndex;
+        private class MinStackElement
+        {
+            public int Min { get; }
+            public int Element { get; }
+
+            public MinStackElement(int element, int min)
+            {
+                Element = element;
+                Min = min;
+            }
+        }
+
+        private Stack<MinStackElement> stack;
 
         /** initialize your data structure here. */
         public MinStack()
         {
-            minimumStack = new Stack<int>();
-            stack = new Stack<int>();
-            minimumStackIndex = new Stack<int>();
+            stack = new Stack<MinStackElement>();
         }
 
         public void Push(int x)
         {
-            stack.Push(x);
-
-            if (minimumStack.Count == 0 || minimumStack.Peek() > x)
-            {
-                minimumStack.Push(x);
-                minimumStackIndex.Push(stack.Count);
-            }
+            int min = stack.Count == 0 ? x : System.Math.Min(stack.Peek().Min, x);
+            stack.Push(new MinStackElement(x, min));
         }
 
         public void Pop()
         {
-            int x = stack.Pop();
-
-            if (x == minimumStack.Peek() && minimumStackIndex.Peek() == stack.Count + 1)
-            {
-                minimumStack.Pop();
-                minimumStackIndex.Pop();
-            }
+            stack.Pop();
         }
 
         public int Top()
         {
-            return stack.Peek();
+            return stack.Peek().Element;
         }
 
         public int GetMin()
         {
-            return minimumStack.Peek();
+            return stack.Peek().Min;
         }
     }
+
+    // Sx = O(3n)
+    //public class MinStack
+    //{
+    //    private Stack<int> minimumStack;
+    //    private Stack<int> stack;
+    //    private Stack<int> minimumStackIndex;
+
+    //    /** initialize your data structure here. */
+    //    public MinStack()
+    //    {
+    //        minimumStack = new Stack<int>();
+    //        stack = new Stack<int>();
+    //        minimumStackIndex = new Stack<int>();
+    //    }
+
+    //    public void Push(int x)
+    //    {
+    //        stack.Push(x);
+
+    //        if (minimumStack.Count == 0 || minimumStack.Peek() > x)
+    //        {
+    //            minimumStack.Push(x);
+    //            minimumStackIndex.Push(stack.Count);
+    //        }
+    //    }
+
+    //    public void Pop()
+    //    {
+    //        int x = stack.Pop();
+
+    //        if (x == minimumStack.Peek() && minimumStackIndex.Peek() == stack.Count + 1)
+    //        {
+    //            minimumStack.Pop();
+    //            minimumStackIndex.Pop();
+    //        }
+    //    }
+
+    //    public int Top()
+    //    {
+    //        return stack.Peek();
+    //    }
+
+    //    public int GetMin()
+    //    {
+    //        return minimumStack.Peek();
+    //    }
+    //}
 }
