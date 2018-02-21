85f32fbb96dfe3f75fa6b702fcabed609adc3a8e
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-20 23:27:51 -0800

    LCD-112: Added solution to Binary Tree has Path Sum problem.

---
+++ C#/Leetcode/BinaryTree/PathSum.cs
@@ -0,0 +1,19 @@
+﻿using LeetcodeSolutions.DataStructures;
+
+namespace LeetcodeSolutions.BinaryTree
+{
+    // Leetcode 112
+    // Submission Detail: https://leetcode.com/submissions/detail/141729452/
+    public class PathSum
+    {
+        // Tx = O(n)
+        // Sx = O(d)    // for call stack
+        public bool HasPathSum(BinaryTreeNode root, int sum)
+        {
+            if (root == null) return false;
+            if (root.Left == null && root.Right == null) return root.Val == sum;
+
+            return HasPathSum(root.Left, sum - root.Val) || HasPathSum(root.Right, sum - root.Val);
+        }
+    }
+}
