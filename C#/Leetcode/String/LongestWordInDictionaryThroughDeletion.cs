2304b16c77563a5153a0b6db8b41344cdb086540
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-21 19:11:24 -0800

    LCd-524: Added solution to Longest Word In Dictionary Through Deletion problem.

---
+++ C#/Leetcode/String/LongestWordInDictionaryThroughDeletion.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace LeetcodeSolutions.String
+{
+    // Leetcode 524
+    // Submission Detail: https://leetcode.com/submissions/detail/141833363/
+    // Comparision problem.
+    public class LongestWordInDictionaryThroughDeletion
+    {
+        // Input: s = "abpcplea", d = ["ale","apple","monkey","plea"]
+        // Output: "apple"
+
+        // Input: s = "abpcplea", d = ["apple","abple","monkey","plea"]
+        // Output: "abple"
+
+        // Intput: s = "abpcplea", d = ["z","y","x","d"]
+        // Output: ""
+
+        // Tx = O(n * k) {n: d.Length, k: s.Length}
+        // Sx = O(k) for longest string
+        public string FindLongestWord(string s, IList<string> d)
+        {
+            string longest = string.Empty;
+
+            foreach (string word in d)
+            {
+                int i = 0;
+
+                // count number of characters in the current dictionary word present in sequence in s.
+                foreach (char c in s)
+                    if (i < word.Length && c == word[i])
+                        i++;
+
+                // check if the full current word in dictionary is a subsequence in s
+                // and if the word length is >= longest length.
+                // = condition is required to check lexicographical order (compareTo in nested if)
+                if (i == word.Length && word.Length >= longest.Length)
+                    if (word.Length > longest.Length || word.CompareTo(longest) < 0)
+                        longest = word;
+            }
+
+            return longest;
+        }
+    }
+}
