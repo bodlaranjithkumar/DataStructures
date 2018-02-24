02298e54de841fc1b3f164961d42b66af0f11d8d
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-24 14:53:32 -0800

    AsseymblyInfo changes after upgrading VS Build to 15.5.7 to fix GITHUB issues.

---
+++ C#/MultiThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hell o World!");
+        }
+    }
+}
