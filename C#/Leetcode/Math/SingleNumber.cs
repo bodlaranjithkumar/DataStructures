46db26c844e24d3fb090b00fd44ab7e0ebe0f154
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-24 22:00:43 -0800

    LCd-136: Added solution to single number problem.

---
+++ C#/Leetcode/Math/SingleNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetcodeSolutions.Math
+{
+    // Leetcode 136
+    // Submission Detail: https://leetcode.com/submissions/detail/142272930/
+    // XOR
+    public class SingleNumber
+    {
+        // Input: [-1,5,3,-1,5] -> 3
+        // Input: [1]   -> 1
+        // Tx = O(n)
+        // Sx = O(1)
+        public int FindSingleNumber(int[] nums)
+        {
+            int result = 0;
+
+            // XOR. 0,1->1 && 1,0->1
+            foreach (int num in nums)
+                result ^= num;
+
+            return result;
+        }
+    }
+}
