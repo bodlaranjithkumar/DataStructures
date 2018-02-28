9490f8d0e1075157b8f6c36ff6f0adcdf5d974dc
rbodla <bodlaranjithkumar@hotmail.com>
2018-02-27 23:20:47 -0800

    LCD-142: Added solution to linked list cycle II problem.

---
+++ C#/Leetcode/LinkedList/LinkedListCycleII.cs
@@ -0,0 +1,41 @@
+﻿using LeetcodeSolutions.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.LinkedList
+{
+    // Leetcode 142
+    // Submission Detail: https://leetcode.com/submissions/detail/142762203/
+    // Slow runner and fast runner technique
+    public class LinkedListCycleII
+    {
+        // Tx = O(n) // O(2n) worst case
+        // Sx = O(1)
+        public ListNode DetectCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.Next != null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+
+                if (fast == slow) break;
+            }
+
+            if (fast == null || fast.Next == null) return null;   // no cycle
+
+            slow = head;
+
+            while (slow != fast)
+            {
+                slow = slow.Next;
+                fast = fast.Next;
+            }
+
+            return slow;
+        }
+    }
+}
