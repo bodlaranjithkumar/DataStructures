910e5b6f9dd22031002e5bf7541e9b5603d9274e
rbodla <bodlaranjithkumar@hotmail.com>
2018-03-04 14:16:47 -0800

    LCD-24: Added solution for swap nodes in pairs (in a Linked List) problem.

---
+++ C#/Leetcode/LinkedList/SwapNodeInPairs.cs
@@ -0,0 +1,55 @@
+﻿using LeetcodeSolutions.DataStructures;
+using System;
+
+namespace LeetcodeSolutions.LinkedList
+{
+    // Leetcode 24
+    // Submission Detail: https://leetcode.com/submissions/detail/143435106/
+    public class SwapNodeInPairs
+    {
+        //public static void Main(string[] args)
+        //{
+        //    ListNode node1 = new ListNode(1);
+        //    ListNode node2 = new ListNode(2);
+        //    ListNode node3 = new ListNode(3);
+        //    ListNode node4 = new ListNode(4);
+
+        //    node1.Next = node2;
+        //    node2.Next = node3;
+        //    node3.Next = node4;
+
+        //    Helper.PrintListNodes(SwapPairs(node1));
+
+        //    Console.ReadLine();
+        //}
+
+        // In place algorithm
+        // Tx = O(n)
+        // Sx = O(1)
+        public static ListNode SwapPairs(ListNode head)
+        {
+            ListNode Prev = new ListNode(0)
+            {
+                Next = head
+            };
+            ListNode ResultNode = Prev;
+            ListNode Current = head;
+
+            while (Current != null && Current.Next != null)
+            {
+                ListNode NextNode = Current.Next;
+                Prev.Next = NextNode;
+
+                ListNode temp = NextNode.Next;
+                NextNode.Next = Current;
+                Current.Next = temp;
+
+                //For next iteration
+                Prev = Current;
+                Current = Current.Next;
+            }
+
+            return ResultNode.Next;
+        }
+    }
+}
