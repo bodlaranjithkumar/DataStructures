2d23cd7817eba91c9e01501312e36271839df617
rbodla <bodlaranjithkumar@hotmail.com>
2018-03-04 17:59:37 -0800

    LCD-168: Added solution to Excel Sheet Column Title problem.

---
+++ C#/Leetcode/Math/ExcelSheetColumnTitle.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace LeetcodeSolutions.Math
+{
+    // Leetcode 168
+    // Submission Detail: https://leetcode.com/submissions/detail/143480793/
+    public class ExcelSheetColumnTitle
+    {
+        // 28 -> AB
+        // 53 -> BA
+        // 100 -> CV
+        // Tx = O(1)
+        // Sx = O(1)
+        public string ConvertToTitle(int n)
+        {
+            StringBuilder s = new StringBuilder();
+
+            while (n > 0)
+            {
+                n--;
+                s.Insert(0, (char)('A' + n % 26));
+                n /= 26;
+            }
+
+            return s.ToString();
+        }
+    }
+}
