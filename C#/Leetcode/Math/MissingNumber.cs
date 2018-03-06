4a4c01ef4b5228c941602979865882abd0339d62
rbodla <bodlaranjithkumar@hotmail.com>
2018-03-05 17:37:09 -0800

    LCD-268: Added solutions to Find Missing Number in the given array.

---
+++ C#/Leetcode/Math/MissingNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetcodeSolutions.Math
+{
+    // Leetcode 268
+    // Submission Detail:
+    //      Sum : https://leetcode.com/submissions/detail/143644300/
+    //      Bit Manipulation: https://leetcode.com/submissions/detail/143645339/
+
+    // Input: [3,0,1] -> 2
+    // Input: [9,6,4,2,3,5,7,0,1] -> 8
+    public class MissingNumber
+    {
+        // Tx = O(n)
+        // Sx = O(1)
+        // Thought: x ^ x = 0
+        public int FindMissingNumberBitManipulation(int[] nums)
+        {
+            int missingNumber = 0, current = 1;
+
+            foreach (int num in nums)
+            {
+                missingNumber ^= current ^ num;
+                current++;
+            }
+
+            return missingNumber;
+        }
+
+        // Tx = O(n)
+        // Sx = O(1)
+        // Possibility of integer overflow exception of big n.
+        public int FindMissingNumber(int[] nums)
+        {
+            int n = nums.Length + 1;
+            int expectedSum = n * (n - 1) / 2;
+
+            int sum = 0;
+            foreach (int num in nums)
+                sum += num;
+
+            return expectedSum - sum;
+        }
+    }
+}
