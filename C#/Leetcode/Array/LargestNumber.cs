20786bbb355f74e856bd13e18403631b06a1bbca
rbodla <bodlaranjithkumar@hotmail.com>
2018-04-22 01:02:53 -0700

    LCD-171: Added solution to Set Matrix Zeros problem.

---
+++ C#/Leetcode/Array/LargestNumber.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetcodeSolutions.Array
+{
+    // Leetcode 171
+    // Submission Detail: https://leetcode.com/submissions/detail/151099674/
+    public class LargestNumber
+    {
+        // Tx = O(nlogn)
+        // Sx = O(n)
+        // Sort
+        public string FormLargestNumber(int[] nums)
+        {
+            string[] numbers = new string[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+                numbers[i] = nums[i] + "";
+            
+            System.Array.Sort(numbers, (str1, str2) =>
+            {
+                var s1 = str1 + str2;
+                var s2 = str2 + str1;
+
+                return s2.CompareTo(s1);
+            });
+
+            // To cover the case of [0,0] -> "0"
+            if (numbers[0] == "0") return "0";
+
+            string largestNumber = string.Join("", numbers);
+
+            return largestNumber;
+        }
+    }
+}
