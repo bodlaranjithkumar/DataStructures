79f9c8e417b3cec338760fa9d6a07f653a2400a3
rbodla <bodlaranjithkumar@hotmail.com>
2018-07-09 19:02:19 -0700

    LCD-819: Added solution to the most common word problem.

---
+++ C#/Leetcode/String/MostCommonWord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LeetcodeSolutions.String
+{
+    // Leetcode 819
+    // Submission Detail: https://leetcode.com/submissions/detail/163003918/
+    public class MostCommonWord
+    {
+        public string FindMostCommonWord(string paragraph, string[] banned)
+        {
+            HashSet<string> bannedWords = new HashSet<string>(banned);
+            string[] words = Regex.Split(paragraph, "[^a-zA-Z]");
+            IDictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            int maxFrequency = 0;
+            foreach (string w in words)
+            {
+                string word = w.ToLower();
+                if (bannedWords.Contains(word) || string.IsNullOrWhiteSpace(word)) continue;
+
+                if (!wordFrequency.ContainsKey(word))
+                    wordFrequency.Add(word, 0);
+
+                wordFrequency[word]++;
+
+                maxFrequency = System.Math.Max(maxFrequency, wordFrequency[word]);
+            }
+
+            string wordWithMaxFrequency = " ";
+            foreach (string word in wordFrequency.Keys)
+            {
+                if (wordFrequency[word] == maxFrequency)
+                    wordWithMaxFrequency = word;
+            }
+
+            return wordWithMaxFrequency;
+        }
+    }
+}
