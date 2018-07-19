1440dafe0f1253e2ca7622af23cd05fcf3ef2a57
rbodla <bodlaranjithkumar@hotmail.com>
2018-07-18 21:12:53 -0700

    LCD-191: Added solution to calculate the number of 1 bits in given uint problem.

---
+++ C#/Leetcode/BitWise/NumberOf1Bits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.BitWise
+{
+    // Leetcode 191
+    // Submission Detail: https://leetcode.com/submissions/detail/164583126/
+    public class NumberOf1Bits
+    {
+        // Ref: https://www.geeksforgeeks.org/count-set-bits-in-an-integer/
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+
+            while (n > 0)
+            {
+                count++;
+                n = n & n - 1;
+            }
+
+            return count;
+        }
+    }
+}
