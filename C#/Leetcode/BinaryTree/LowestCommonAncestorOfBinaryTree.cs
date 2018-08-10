9acb4e8aedf307c8af2b8ed6c4e382d5214a221b
rbodla <bodlaranjithkumar@hotmail.com>
2018-08-10 15:42:21 -0700

    LCD-236: Added solution to Lowest Common Ancestor of a Binary Tree.

--- C#/Leetcode/BinaryTree/LowestCommonAncestorOfBinaryTree.cs
+++ C#/Leetcode/BinaryTree/LowestCommonAncestorOfBinaryTree.cs
@@ -3,7 +3,7 @@
 namespace LeetcodeSolutions.BinaryTree
 {
     // Leetcode 236
-    class LowestCommonAncestorOfBinaryTree
+    public class LowestCommonAncestorOfBinaryTree
     {
         //      _______3______
         //     /              \
@@ -12,22 +12,39 @@ namespace LeetcodeSolutions.BinaryTree
         // 6      _2       0       8
         //       /  \
         //       7   4
-         
+
+        // submission detail: https://leetcode.com/submissions/detail/168752862/
+        // ref: https://www.youtube.com/watch?v=13m9ZCB8gjw
+        // Tx: O(n)
+        // Sx: O(d)
+        public BinaryTreeNode LowestCommonAncestor(BinaryTreeNode root, BinaryTreeNode p, BinaryTreeNode q)
+        {
+            if (root == null || root == p || root == q) return root;
+
+            BinaryTreeNode left = LowestCommonAncestor(root.Left, p, q);
+            BinaryTreeNode right = LowestCommonAncestor(root.Right, p, q);
+
+            if (left != null && right != null) return root;
+            if (left == null && right == null) return null;
+            return left ?? right;
+        }
+
+
         // Explanation: Given root node recursively call the left and right sub trees.
         //              if the root node is either equal to p or q, then it is not required to check
         //              it's sub tree since if the 2nd node exists in it's subtrees, LCA is the root node.
         //              So, instead check rest of the other subtrees to make sure that the other
         //              node doesn't exist in that.
-        
-            // Tx = O(n)
-        // Sx = O(n) for recursive call stack
-        public BinaryTreeNode LowestCommonAncestor(BinaryTreeNode root, BinaryTreeNode p, BinaryTreeNode q)
+
+        // Tx = O(n)
+        // Sx = O(d) for recursive call stack
+        public BinaryTreeNode LowestCommonAncestor1(BinaryTreeNode root, BinaryTreeNode p, BinaryTreeNode q)
         {
             if (root == null || root == p || root == q)
                 return null;
 
-            BinaryTreeNode left = LowestCommonAncestor(root.Left, p, q);
-            BinaryTreeNode right = LowestCommonAncestor(root.Right, p, q);
+            BinaryTreeNode left = LowestCommonAncestor1(root.Left, p, q);
+            BinaryTreeNode right = LowestCommonAncestor1(root.Right, p, q);
 
             return left != null && right != null
                         ? root :
