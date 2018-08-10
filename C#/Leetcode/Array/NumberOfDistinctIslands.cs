57230c117a4bd709d76aa006a29598f82ba5414f
rbodla <bodlaranjithkumar@hotmail.com>
2018-08-09 22:03:09 -0700

    LCD-694: Added solution to number of distinct islands problem. Solved using DFS.

---
+++ C#/Leetcode/Array/NumberOfDistinctIslands.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.Array
+{
+    public class NumberOfDistinctIslands
+    {
+        // Leetcode 694
+        // Depth-First Traversal
+        //public static void Main(string[] args)
+        //{
+        //    NumberOfDistinctIslands distinctIslands = new NumberOfDistinctIslands();
+
+        //    int[,] grid1 = { { 1, 1, 0, 0 },
+        //                     { 1, 1, 0, 0 },
+        //                     { 0, 0, 1, 1 },
+        //                     { 0, 0, 1, 1 } };
+
+        //    Console.WriteLine(distinctIslands.NumDistinctIslands(grid1));   //1
+
+        //    int[,] grid2 = { { 1, 1, 0, 1, 1 },
+        //                     { 1, 0, 0, 0, 0 },
+        //                     { 0, 0, 0, 0, 1 },
+        //                     { 1, 1, 0, 1, 1 } };
+
+        //    Console.WriteLine(distinctIslands.NumDistinctIslands(grid2));   //3
+
+        //    Console.ReadKey();
+        //}
+
+        // Idea: Use DFS. This is an extension to number of islands problem.
+        //      This problem requires comparing the indexes of all the islands. If there are n islands, nP2 comparisions.
+        //      Optimal way is to create a unique identifier - string that represents the boundaries of the island
+        //      Boundaries are calculated relative to base coordinates (baseX, baseY). Now the problem becomes
+        //      finding the number of these unique strings which can be done using a hashset.
+
+        int m, n;
+        public int NumDistinctIslands(int[,] grid)
+        {
+            if (grid == null || grid.GetLength(0) == 0 || grid.GetLength(1) == 0)
+                return 0;
+
+            m = grid.GetLength(0);
+            n = grid.GetLength(1);
+
+            HashSet<string> distinctIslands = new HashSet<string>();
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i, j] == 1)
+                    {
+                        StringBuilder sb = new StringBuilder();
+
+                        NumDistinctIslands(grid, i, j, i, j, sb);
+
+                        if (!distinctIslands.Contains(sb.ToString()))
+                            distinctIslands.Add(sb.ToString());
+                    }
+                }
+            }
+
+            return distinctIslands.Count;
+        }
+
+        private void NumDistinctIslands(int[,] grid, int i, int j, int baseX, int baseY, StringBuilder sb)
+        {
+            if (i < 0 || j < 0 || i >= m || j >= n || grid[i, j] == 0)
+                return;
+
+            if (sb.Length > 0) sb.Append("_");
+
+            sb.Append(i - baseX).Append("_").Append(j - baseY);
+            grid[i, j] = 0;
+
+            NumDistinctIslands(grid, i + 1, j, baseX, baseY, sb);   // go right
+            NumDistinctIslands(grid, i - 1, j, baseX, baseY, sb);   // go left
+            NumDistinctIslands(grid, i, j + 1, baseX, baseY, sb);   // go down
+            NumDistinctIslands(grid, i, j - 1, baseX, baseY, sb);   // go up
+        }
+    }
+}
---
+++ C#/Leetcode/String/PalindromicSubstringsOfString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetcodeSolutions.String
+{
+    public class PalindromicSubstringsOfString
+    {
+        //public static void Main(string[] args)
+        //{
+        //    PalindromicSubstringsOfString ps = new PalindromicSubstringsOfString();
+        //    Helper.PrintListElements<string>(ps.PalindromicSubstrings("pop this tacocat seems nice at noon"));
+
+        //    Console.ReadKey();
+        //}
+
+        private IList<string> palindromicSubstrings = new List<string>();
+
+        public IList<string> PalindromicSubstrings(string s)
+        {
+            if (s != null)
+            {
+                for (int i = 0; i < s.Length-1; i++)
+                {
+                    PalindromicSubstrings(s, i, i + 1);
+                }
+            }
+
+            return palindromicSubstrings;
+        }
+
+        private void PalindromicSubstrings(string s, int left, int right)
+        {
+            while(left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                string str = s.Substring(left, right-left+1);
+
+                if (palindromicSubstrings.Contains(str))
+                    palindromicSubstrings.Add(str);
+
+                left--;
+                right++;
+            }
+        }
+    }
+}
