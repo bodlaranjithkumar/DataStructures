c5ce509b8bbee5902b1347f1ae66e4699555d88a
rbodla <bodlaranjithkumar@hotmail.com>
2018-08-12 20:16:17 -0700

    LCD-297: Added solution for Serialize and Deserialize Binary Tree - Same solution as BST.

--- C#/Leetcode/BinaryTree/SerializeAndDeserializeBinaryTree.cs
+++ C#/Leetcode/BinaryTree/SerializeAndDeserializeBinaryTree.cs
@@ -6,7 +6,9 @@ using System.Text;
 namespace LeetcodeSolutions.BinaryTree
 {
     // Leetcode 297
-    class SerializeAndDeserializeBinaryTree
+    // Same solution as Serialize and Deserialize Binary Search Tree
+    // Submission Detail: https://leetcode.com/submissions/detail/169155362/
+    public class SerializeAndDeserializeBinaryTree
     {
         static char Delimiter = ',';
 
@@ -24,83 +26,83 @@ namespace LeetcodeSolutions.BinaryTree
 
         // Serialize using , as the delimiter
         // Encodes a tree to a single string.
-        public static string Serialize(BinaryTreeNode root)
-        {
-            StringBuilder serializedTree = new StringBuilder();
-            Queue<BinaryTreeNode> nodes = new Queue<BinaryTreeNode>();
+        //public static string Serialize(BinaryTreeNode root)
+        //{
+        //    StringBuilder serializedTree = new StringBuilder();
+        //    Queue<BinaryTreeNode> nodes = new Queue<BinaryTreeNode>();
 
-            if (root != null)
-                nodes.Enqueue(root);
+        //    if (root != null)
+        //        nodes.Enqueue(root);
 
-            while (nodes.Count > 0)
-            {
-                BinaryTreeNode node = nodes.Dequeue();
+        //    while (nodes.Count > 0)
+        //    {
+        //        BinaryTreeNode node = nodes.Dequeue();
 
-                if (node != null)
-                {
-                    serializedTree.Append(node.Val);
-                    nodes.Enqueue(node.Left);
-                    nodes.Enqueue(node.Right);
-                }
-                else
-                {
-                    serializedTree.Append("null");
-                }
+        //        if (node != null)
+        //        {
+        //            serializedTree.Append(node.Val);
+        //            nodes.Enqueue(node.Left);
+        //            nodes.Enqueue(node.Right);
+        //        }
+        //        else
+        //        {
+        //            serializedTree.Append("null");
+        //        }
 
-                serializedTree.Append(Delimiter);
-            }
+        //        serializedTree.Append(Delimiter);
+        //    }
 
-            // Remove the delimiter appended in the last
-            // TODO: Add logic in the loop to avoid appending this.
-            if (serializedTree.Length > 0)
-                serializedTree.Remove(serializedTree.Length - 1, 1);
+        //    // Remove the delimiter appended in the last
+        //    // TODO: Add logic in the loop to avoid appending this.
+        //    if (serializedTree.Length > 0)
+        //        serializedTree.Remove(serializedTree.Length - 1, 1);
 
-            return serializedTree.ToString();
-        }
+        //    return serializedTree.ToString();
+        //}
 
         // Decodes your encoded data to tree.
-        public static BinaryTreeNode Deserialize(string data)
-        {
-            BinaryTreeNode root = null;
+        //public static BinaryTreeNode Deserialize(string data)
+        //{
+        //    BinaryTreeNode root = null;
 
-            Queue<BinaryTreeNode> nodes = new Queue<BinaryTreeNode>();
-            nodes.Enqueue(root);
+        //    Queue<BinaryTreeNode> nodes = new Queue<BinaryTreeNode>();
+        //    nodes.Enqueue(root);
 
-            StringBuilder currentNodeValue = new StringBuilder();
+        //    StringBuilder currentNodeValue = new StringBuilder();
 
-            for (int index = 0; index < data.Length; index++)
-            {
-                char currentChar = data[index];
+        //    for (int index = 0; index < data.Length; index++)
+        //    {
+        //        char currentChar = data[index];
                 
-                if (currentChar != Delimiter)
-                {
-                    currentNodeValue.Append(currentChar);
-                }
-                else
-                {
-                    BinaryTreeNode node = nodes.Dequeue();
+        //        if (currentChar != Delimiter)
+        //        {
+        //            currentNodeValue.Append(currentChar);
+        //        }
+        //        else
+        //        {
+        //            BinaryTreeNode node = nodes.Dequeue();
                     
-                    if (int.TryParse(currentNodeValue.ToString(), out int value))
-                    {
-                        if (root == null)
-                        {
-                            root = new BinaryTreeNode(value);
-                            node = root;
-                        }
-                        else
-                        {
-                            node = new BinaryTreeNode(value);
-                        }
+        //            if (int.TryParse(currentNodeValue.ToString(), out int value))
+        //            {
+        //                if (root == null)
+        //                {
+        //                    root = new BinaryTreeNode(value);
+        //                    node = root;
+        //                }
+        //                else
+        //                {
+        //                    node = new BinaryTreeNode(value);
+        //                }
 
-                        nodes.Enqueue(node.Left);
-                        nodes.Enqueue(node.Right);
-                    }
+        //                nodes.Enqueue(node.Left);
+        //                nodes.Enqueue(node.Right);
+        //            }
 
-                    currentNodeValue.Clear();
-                }
-            }
+        //            currentNodeValue.Clear();
+        //        }
+        //    }
 
-            return root;
-        }
+        //    return root;
+        //}
     }
 }
