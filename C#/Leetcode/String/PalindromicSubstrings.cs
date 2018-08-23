62d4032db9052560adfa61d6d8656b4e8b27057f
rbodla <bodlaranjithkumar@hotmail.com>
2018-08-22 19:13:05 -0700

    LCD-647: Added solution for palindromic substring problem.

---
+++ C#/Leetcode/String/PalindromicSubstrings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.String
+{
+    // Leetcode 647 - https://leetcode.com/problems/palindromic-substrings/description/
+    // Submission Detail - https://leetcode.com/submissions/detail/171122845/
+    // ref: https://leetcode.com/problems/palindromic-substrings/discuss/105688/Very-Simple-Java-Solution-with-Detail-Explanation
+
+    public class PalindromicSubstrings
+    {
+
+        // case sensitive
+        int count = 0;
+        public int CountSubstrings(string s)
+        {
+            if (s == null || s.Length == 0)
+                return 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                CountSubstrings(s, i, i);   // To cover the case of palindromic substring of odd length.
+                CountSubstrings(s, i, i + 1); // To cover the case of palindromic substring of even length.
+            }
+
+            return count;
+        }
+
+        private void CountSubstrings(string s, int start, int end)
+        {
+            while (start >= 0 && end < s.Length && s[start] == s[end])
+            {
+                count++;
+                start--;
+                end++;
+            }
+        }
+    }
+}
