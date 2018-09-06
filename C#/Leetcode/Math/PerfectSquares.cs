ad280789e2ad68396c05a767d1000da8e89c573c
rbodla <bodlaranjithkumar@hotmail.com>
2018-09-05 23:17:36 -0700

    Runtime optimized for Perfect Square problem.

--- C#/Leetcode/Math/PerfectSquares.cs
+++ C#/Leetcode/Math/PerfectSquares.cs
@@ -1,7 +1,7 @@
 ﻿namespace LeetcodeSolutions.Math
 {
     // Leetcode 279 - https://leetcode.com/problems/perfect-squares
-    // Submission Detail - https://leetcode.com/submissions/detail/141588006/
+    // Submission Detail - https://leetcode.com/submissions/detail/174152816/
     // Bottom-Up Dynamic Programming. Similar to Coin change problem.
 
     public class PerfectSquares
@@ -10,17 +10,11 @@
         public int NumSquares(int n)
         {
             int[] numSquares = new int[n + 1];
-
+            
             for (int i = 1; i <= n; i++)
             {
-                if (i * i == n)
-                    return 1;   
-                else
-                    numSquares[i] = n + 1;   // Initialize to n+1 as n cannot have more than n perfect squares.
-            }
+                numSquares[i] = n + 1;   // Initialize to n+1 as n cannot have more than n perfect squares.
 
-            for (int i = 1; i <= n; i++)
-            {
                 // Run for each perfect square less than i.
                 for (int j = 1; j * j <= i; j++)
                 {
