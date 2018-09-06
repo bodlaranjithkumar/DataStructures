483d8ece4c650818d7f4a203361623f95c6eea8d
rbodla <bodlaranjithkumar@hotmail.com>
2018-09-05 22:15:18 -0700

    Added solution for implement stack using linked list.

---
+++ C#/Leetcode/Algorithms/StackUsingLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using LeetcodeSolutions.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.Algorithms
+{
+    public class StackUsingLinkedList
+    {
+        ListNode head;
+        int count = 0;
+
+        public void Push(int item)
+        {
+            ListNode newHead = new ListNode(item);
+
+            if (count == 0)
+            {
+                head = newHead;
+            }
+            else
+            {
+                newHead.Next = head;
+                head = newHead;
+            }
+
+            count++;
+        }
+       
+        public int Pop()
+        {
+            if (count == 0)
+                throw new InvalidOperationException("Stack is Empty.");
+
+            var popNode = head;
+            head = head.Next;
+
+            count--;
+
+            return popNode.Val;
+        }
+
+        public int Peek()
+        {
+            if (count == 0)
+                throw new InvalidOperationException("Stack is Empty.");
+
+            return head.Val;
+        }
+    }
+}
