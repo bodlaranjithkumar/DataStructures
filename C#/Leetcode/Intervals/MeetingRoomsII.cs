e55c887848897a62df3c11052a188664f1b721dd
rbodla <bodlaranjithkumar@hotmail.com>
2018-10-10 21:36:46 -0700

    Added readable solution for meeting rooms II problem.

--- C#/Leetcode/Intervals/MeetingRoomsII.cs
+++ C#/Leetcode/Intervals/MeetingRoomsII.cs
@@ -1,42 +1,47 @@
 ﻿using LeetcodeSolutions.DataStructures;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LeetcodeSolutions.Intervals
 {
     // Leetcode 253
-    // Explanation: http://www.cnblogs.com/grandyang/p/5244720.html
+    // Submission Detail: https://leetcode.com/submissions/detail/181966142/
+
     // Input: [[15,20],[0,30],[35,40]] -> 2
     // Input: [[15,40],[15,20],[0,30]] -> 3
     public class MeetingRoomsII
     {
-        // TODO: Optimal solution
         // Tx = O(nlogn)
         // Sx = O(n)
         public int MinMeetingRooms(Interval[] intervals)
         {
             int length = intervals.Length;
-            int[] start = new int[length];
-            int[] end = new int[length];
+
+            int[] startTimes = new int[length];
+            int[] endTimes = new int[length];
 
             for (int i = 0; i < length; i++)
             {
-                start[i] = intervals[i].start;
-                end[i] = intervals[i].end;
+                startTimes[i] = intervals[i].start;
+                endTimes[i] = intervals[i].end;
             }
 
-            System.Array.Sort(start);
-            System.Array.Sort(end);
+            System.Array.Sort(startTimes);
+            System.Array.Sort(endTimes);
 
-            int minMeetings = 0, endPos = 0;
-            for (int i = 0; i < start.Length; i++)
+            int startIndex = 0, endIndex = 0, minMeetingRooms = 0;
+
+            while (startIndex < length)
             {
-                if (start[i] < end[endPos]) minMeetings++;
-                else endPos++;
+                if (startTimes[startIndex] >= endTimes[endIndex])
+                {
+                    minMeetingRooms--;  // Use one of the meeting rooms ended.
+                    endIndex++;
+                }
+
+                minMeetingRooms++;
+                startIndex++;
             }
 
-            return minMeetings;
+            return minMeetingRooms;
         }
     }
 }
