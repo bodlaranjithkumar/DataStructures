3866d80962af40b0c8223a681224325f1eb227fb
rbodla <bodlaranjithkumar@hotmail.com>
2018-10-20 11:53:52 -0700

    LCD-61: Added solution for rotate linked list by k times problem.

---
+++ C#/Leetcode/LinkedList/RotateList.cs
@@ -0,0 +1,57 @@
+﻿using LeetcodeSolutions.DataStructures;
+using System;
+
+namespace LeetcodeSolutions.LinkedList
+{
+    // Leetcode 61 - https://leetcode.com/problems/rotate-list/description/
+    // Submission Detail - https://leetcode.com/submissions/detail/184090592/
+
+    public class RotateList
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null || head.Next == null)
+                return head;
+
+            int totalNodes = 0;
+            ListNode current = head, lastNode = null;
+
+            // Count the total number of nodes in the list.
+            while (current != null)
+            {
+                if (current.Next == null)
+                    lastNode = current;
+
+                current = current.Next;
+
+                totalNodes++;
+            }
+
+            // If total nodes = 5, k = 7, now rotating 2 times will suffice.        
+            k = k % totalNodes;
+
+            if (k == 0)
+                return head;
+
+            int newHeadIndex = totalNodes - k; // The new head index
+            ListNode newHead;
+            current = head;
+
+            // Find the node before the newHeadIndex
+            while (newHeadIndex > 1)
+            {
+                current = current.Next;
+                newHeadIndex--;
+            }
+
+            // Set the node before the newHeadIndex next to null.
+            newHead = current.Next;
+            current.Next = null;
+
+            // Set the old head as the next node of the lastNode present before rotating.
+            lastNode.Next = head;
+
+            return newHead;
+        }
+    }
+}
