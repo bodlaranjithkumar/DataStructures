bf3e59319c4c7b026f262557e77f7374e55dadb2
rbodla <bodlaranjithkumar@hotmail.com>
2018-11-14 22:37:29 -0800

    Leetcode 204: Added solution for find total primes less than n problem.

---
+++ C#/Leetcode/Math/CountPrimes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolutions.Math
+{
+    // Leecode 204 - https://leetcode.com/problems/count-primes/description/
+    // Submission Detail - https://leetcode.com/submissions/detail/189692073/
+
+    // Sieve of Eratosthenes
+    // Explanation: https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+
+    // Potential Follow-Up Question: Find all the primes less than n
+    // Iterate through the IsNotPrime boolean array and add the index+1 value to the list.
+
+    public class CountPrimes
+    {
+        // Tx = O(n log(logn))
+        // Sx = O(n)
+
+        public int CountPrimesLessThanN(int n)
+        {
+            bool[] isNotPrime = new bool[n];
+            int count = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (!isNotPrime[i])
+                { // i.e. i is prime
+                    count++;
+
+                    for (int j = 2; j * i < n; j++)
+                    {
+                        isNotPrime[j * i] = true;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
