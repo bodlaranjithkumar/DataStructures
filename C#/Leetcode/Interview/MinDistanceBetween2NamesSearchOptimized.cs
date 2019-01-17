8e297ee321befa96d9c92e9f7224c0bce4b7a94e
rbodla <bodlaranjithkumar@hotmail.com>
2019-01-16 23:52:01 -0800

    Add solution for find min distance between words in a list.

---
+++ C#/Leetcode/Interview/MinDistanceBetween2NamesSearchOptimized.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace LeetcodeSolutions.Interview
+{
+    // Given a length list of names, find the minimum distance between given 2 names optimizing for find.
+
+    // The given list has many names.
+    // The names to search exist in the list.
+
+    public class MinDistanceBetween2NamesSearchOptimized
+    {
+        public MinDistanceBetween2NamesSearchOptimized(IList<string> names)
+        {
+            PreProcess(names);
+        }
+
+        readonly IDictionary<string, IList<int>> nameIndexes = new Dictionary<string, IList<int>>();
+
+        private void PreProcess(IList<string> names)
+        {
+            for(int i=0; i<names.Count; i++)
+            {
+                var name = names[i];
+
+                if (!nameIndexes.ContainsKey(name))
+                    nameIndexes[name] = new List<int>();
+
+                nameIndexes[name].Add(i);
+            }
+        }
+
+        // Tx = O(m + n) {m: Occurrences of name1, n: Occurrences of name2}
+        // Sx = O(1)
+
+        public int MinDistance(string name1, string name2)
+        {
+            IList<int> name1Indexes = nameIndexes[name1];
+            IList<int> name2Indexes = nameIndexes[name2];
+
+            int index1 = 0, index2 = 0, minDistance = int.MaxValue;
+
+            while(index1 < name1Indexes.Count && index2 < name2Indexes.Count)
+            {
+                minDistance = System.Math.Min(minDistance, 
+                                System.Math.Abs(name1Indexes[index1] - name2Indexes[index2]));
+
+                if (index1 < index2)
+                    index1++;
+                else
+                    index2++;
+            }
+
+            return minDistance;
+        }
+    }
+}
---
+++ C#/Leetcode/Interview/MinDistanceBetween2Words.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetcodeSolutions.Interview
+{
+    // If names exist - return the shortest path
+    // If either of the names doesn't exist - return -1
+    // If both the names are equal - return 0
+    // The list of names is initialized and non-empty.
+    // Case-sensitive 
+
+    // Input List: A, B, C, A, D, T, F, G, H, T, A
+    // A, T => 1
+    // A, A => 0
+    // A, Z => -1
+
+    public class MinDistanceBetween2Words
+    {
+        // Tx = O(n) {n: Count of names list}
+        // Sx = O(1)
+        public int MinDistance(IList<string> names, string name1, string name2)
+        {
+            int length = names.Count;
+            int name1LatestIndex = length, name2LatestIndex = length, minDistance = length;
+
+            for(int i=0; i<length; i++)
+            {
+                string current = names[0];
+
+                if (current.CompareTo(name1) == 0)
+                    name1LatestIndex = i;
+
+                // Not using else if covers the case where both the names are the same.
+                if (current.CompareTo(name2) == 0)
+                    name2LatestIndex = i;
+
+                if (name1LatestIndex != length && name2LatestIndex != length)
+                    minDistance = System.Math.Min(minDistance, System.Math.Abs(name1LatestIndex - name2LatestIndex));
+            }
+
+            if (name1LatestIndex == length || name2LatestIndex == length)
+                return -1;
+
+            return minDistance;
+        }
+    }
+}
