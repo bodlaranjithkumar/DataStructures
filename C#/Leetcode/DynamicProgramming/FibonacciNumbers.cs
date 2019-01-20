12eb75f24d06059dadec1f1f1904ea0056dcb315
rbodla <bodlaranjithkumar@hotmail.com>
2019-01-20 11:44:34 -0800

    LCD-509: Added solution for find the Nth fibonacci number.

---
+++ C#/Leetcode/DynamicProgramming/FibonacciNumbers.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetcodeSolutions.DynamicProgramming
+{
+    // Leetcode 509: https://leetcode.com/problems/fibonacci-number/description/
+    // Submission Detail: https://leetcode.com/submissions/detail/202480047/
+
+    public class FibonacciNumbers
+    {
+        // Bottom-up Dynamic Programming
+        // Tx = O(N)
+        // Sx = O(1)
+        public int Fib(int N)
+        {
+            if (N < 2) return N;
+
+            int prevprev = 0, prev = 1, nthfib = 0;
+
+            for (int i = 1; i < N; i++)
+            {
+                nthfib = prevprev + prev;
+                prevprev = prev;
+                prev = nthfib;
+            }
+
+            return nthfib;
+        }
+    }
+}
--- C#/Leetcode/DynamicProgramming/SubsetSum.cs
+++ C#/Leetcode/DynamicProgramming/SubsetSum.cs
@@ -25,17 +25,24 @@
             bool[,] dp = new bool[rows, targetSum + 1];
 
             for (int i = 0; i < rows; i++)
+            {
                 for (int j = 0; j <= targetSum; j++)
+                {
                     if (j == 0 || (i == 0 && nums[i] == j))
+                    {
                         dp[i, j] = true;
+                    }
                     else if (i > 0)
                     {
                         dp[i, j] = dp[i - 1, j];
 
                         if (j >= nums[i])
-                            dp[i, j] = dp[i,j] || dp[i - 1, j - nums[i]];
+                        {
+                            dp[i, j] = dp[i, j] || dp[i - 1, j - nums[i]];
+                        }
                     }
-
+                }
+            }
 
             return dp[rows - 1, targetSum];
         }
