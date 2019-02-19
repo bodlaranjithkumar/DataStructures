51b954458920343f8c7ffb34a6a4225f396fe9b6
rbodla <bodlaranjithkumar@hotmail.com>
2019-02-18 22:21:49 -0800

    LCD-518: Added solution for coin change 2 problem.

---
+++ C#/Leetcode/DynamicProgramming/CoinChange2.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetcodeSolutions.DynamicProgramming
+{
+    // Leetcode 518 - https://leetcode.com/problems/coin-change-2/description/
+    // Submission Detail - https://leetcode.com/submissions/detail/208999305/
+    // Bottom-Up Dynamic Programming
+
+    public class CoinChange2
+    {
+        public int Change(int amount, int[] coins)
+        {
+            int[] totalWays = new int[amount + 1];
+            totalWays[0] = 1;
+
+            foreach (int coin in coins)
+            {
+                for (int i = 1; i <= amount; i++)
+                {
+                    if (i - coin >= 0)
+                    {
+                        totalWays[i] += totalWays[i - coin];
+                    }
+                }
+            }
+
+            return totalWays[amount];
+        }
+    }
+}
